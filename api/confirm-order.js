a30c58366745ea09626ab8fd3fb9f7fa36c732a0
Shahrair Rezwan <shahrair701@gmail.com>
2020-06-06 00:14:45 +0600

    added cognito user pool and identity pool

--- api/confirm-order.js
+++ api/confirm-order.js
@@ -1,54 +1,55 @@
-import * as dynamoDbLib from "../libs/dynamodb-lib";
-import { success, failure } from "../libs/response-lib";
+// import * as dynamoDbLib from "../libs/dynamodb-lib";
+// import { success, failure } from "../libs/response-lib";
 
-export async function main(event, context) {
+// export async function main(event, context) {
 
-  try {
-    const data = JSON.parse(event.body);
-    const params;
-    params = {
-      TransactItems: [{
-        Put: {
-          TableName: process.env.tableName,
-          order_id: data.order_id,
-          created_at: Date.now(),
-          orientation: data.orientation,
-          pk: data.order_id,
-          sk: data.orientation,
+//   try {
+//     const data = JSON.parse(event.body);
 
-          ConditionExpression: "order_id <> :oi ",
-          ExpressionAttributeValues: {
-            ":oi": data.order_id
-          }
-        }
-      }, {
-        Update: {
-          TableName: process.env.tableName,
-          Item: element,
-          Key: {
-            pk: element.pk,
-            sk: element.sk
-          },
-          UpdateExpression: "SET #status = :status",
-          ExpressionAttributeNames: {
-            "#status": 'status'
-          },
-          ExpressionAttributeValues: {
-            ":status": element.status
-          },
-          ReturnValues: "ALL_NEW"
-        }
-      }]
-    };
+//     const params = null;
+//     params = {
+//       TransactItems: [{
+//         Put: {
+//           TableName: process.env.tableName,
+//           order_id: data.order_id,
+//           created_at: Date.now(),
+//           orientation: data.orientation,
+//           pk: data.order_id,
+//           sk: data.orientation,
 
-    await dynamoDbLib.call("transactWrite", params);
+//           ConditionExpression: "order_id <> :oi ",
+//           ExpressionAttributeValues: {
+//             ":oi": data.order_id
+//           }
+//         }
+//       }, {
+//         Update: {
+//           TableName: process.env.tableName,
+//           Item: element,
+//           Key: {
+//             pk: element.pk,
+//             sk: element.sk
+//           },
+//           UpdateExpression: "SET #status = :status",
+//           ExpressionAttributeNames: {
+//             "#status": 'status'
+//           },
+//           ExpressionAttributeValues: {
+//             ":status": element.status
+//           },
+//           ReturnValues: "ALL_NEW"
+//         }
+//       }]
+//     };
+
+//     await dynamoDbLib.call("transactWrite", params);
 
 
-    return success({
-      data: params.TransactItems,
-      isExecuted: true
-    });
-  } catch (e) {
-    return failure({ status: false, error: e });
-  }
-}
+//     return success({
+//       data: params.TransactItems,
+//       isExecuted: true
+//     });
+//   } catch (e) {
+//     return failure({ status: false, error: e });
+//   }
+// }
